8d6aeb718888680c80351b713275438d9f1820dd
congnguyen <cong.nguyenquoc01@icloud.com>
2022-07-14 09:32:29 +0700

    lab7

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Main from "./component/MainComponent";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+import "./App.css";
+
+const store = ConfigureStore();
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
